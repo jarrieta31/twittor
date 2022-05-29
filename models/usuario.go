47b00598d7beb3ad2844ccf3b59282bd4894ac7f
Julio Arrieta <julioarrieta23@gmail.com>
2022-05-29 14:23:52 -0300

    corrigiendo usuarios

--- models/usuario.go
+++ models/usuario.go
@@ -8,15 +8,15 @@ import (
 
 /*Usuario es el modelo de usuario de la base de MongoDB*/
 type Usuario struct {
-	ID              primitive.ObjectID `bson: "_id,omitemty" json:"id"`
-	Nombre          string             `bson: "nombre" json:"nombre, omitemty"`
-	Apellidos       string             `bson: "apellidos" json:"apellidos, omitemty"`
-	FechaNacimiento time.Time          `bson: "fechaNacimiento" json:"fechaNacimeinto, omitemty"`
+	ID              primitive.ObjectID `bson: "_id,omitempty" json:"id"`
+	Nombre          string             `bson: "nombre" json:"nombre, omitempty"`
+	Apellidos       string             `bson: "apellidos" json:"apellidos, omitempty"`
+	FechaNacimiento time.Time          `bson: "fechaNacimiento" json:"fechaNacimiento, omitempty"`
 	Email           string             `bson: "email" json:"email"`
-	Password        string             `bson: "password" json:"password, omitemty"`
-	Avatar          string             `bson: "avatar" json:"avatar, omitemty"`
-	Banner          string             `bson: "banner" json:"banner, omitemty"`
-	Biografia       string             `bson: "biografia" json:"biografia, omitemty"`
-	Ubicacion       string             `bson: "ubicacion" json:"ubicacion, omitemty"`
-	SitioWeb        string             `bson: "sitioWeb" json:"sitioWeb, omitemty"`
+	Password        string             `bson: "password" json:"password, omitempty"`
+	Avatar          string             `bson: "avatar" json:"avatar, omitempty"`
+	Banner          string             `bson: "banner" json:"banner, omitempty"`
+	Biografia       string             `bson: "biografia" json:"biografia, omitempty"`
+	Ubicacion       string             `bson: "ubicacion" json:"ubicacion, omitempty"`
+	SitioWeb        string             `bson: "sitioWeb" json:"sitioWeb, omitempty"`
 }
